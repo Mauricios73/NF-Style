1fb382584e775565afb891303ebf59799a71665b
Mauricios73 <mauricioportela73@gmail.com>
2023-08-17 01:12:31 -0300

    feat: add navbar, about, imageSlider

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import './App.scss' 
+import './styles/nav.scss'
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import ImageSlider from './components/imageSlider';
+
+const App = () => {
+    return (
+      <main>
+        <nav className="nav">
+          <ul>
+            <li><a href="#" className="current">Home</a></li>
+            <li><a href="#container-section1">About</a></li>
+            <li><a href="#container-section2">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+
+          <div className="container">
+            <header>
+              <div className='imglogo'>
+                <img
+                  src="./barbershop.png"
+                  alt="barbershop"
+                  height={120}
+                  width={260}
+                />
+              </div>
+              <div className='namelogo'>
+                <h1>
+                  <i>NF Style</i>
+                </h1>
+                <p>Transformando homens comuns em verdadeiros cavalheiros!</p>
+              </div>
+            </header>
+          
+            <button className="btn" id="openModalButton">
+              Agenda
+            </button>          
+          </div>
+
+          <section id="container-section1">
+            <Section1 />          
+          </section>
+
+          <section id="container-section2">
+            <Section2 />
+          </section>          
+            <ImageSlider />
+
+
+        {/* <script src="script.tsx"></script> */}
+      </main>
+    )
+}
+
+export default App;
---
+++ src/components/Section1.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/section1.scss'
+
+const Section1: React.FC = () => {
+  return (
+    <div>
+      <section className='container-section1'>
+        <h1>Você está procurando por uma experiência que vai além das suas expectativas? Então bem-vindo à
+          NF  STYLE</h1>
+        <div className='about'>
+          <h2>New</h2>
+          <p>Novo visual, o lugar onde estilo e tradição se encontram!</p>
+          <h2>Fade</h2>
+          <p>O visual do degrade como você nunca viu.</p>
+          <h2>Style</h2>
+          <p>Desde cortes de cabelo modernos e estilos clássicos até barbas perfeitamente aparadas.</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Section1;
