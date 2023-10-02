459095f572ba55501c03ba28effdc3b17057f163
Mauricios73 <mauricioportela73@gmail.com>
2023-10-02 18:11:32 -0300

    fix: ajustes em Nav e Section2

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,14 +4,12 @@ import { Link } from 'react-scroll';
 
 const Nav: React.FC = () => {
   return (
-    <nav className="nav">
       <ul>
         <li><Link to="container" className="current" smooth={true} duration={500}>Home</Link></li>
         <li><Link to="container-section1" smooth={true} duration={500}>About</Link></li>
         <li><Link to="container-section2" smooth={true} duration={500}>Services</Link></li>
         <li><Link to="container-section4" smooth={true} duration={500}>Contact</Link></li>
       </ul>
-    </nav>
   );
 };
 
