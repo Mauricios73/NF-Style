85864afe2bc77face97488b23f8c0f3e56461def
Mauricios73 <mauricioportela73@gmail.com>
2023-10-03 00:21:24 -0300

    fix: href para pagina principal

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 const Nav: React.FC = () => {
   return (
     <ul>
-      <li><a href='http://localhost:5173' className='current'>Home</a></li>
+      {/* <li><a href='http://localhost:5173' className='current'>Home</a></li> */}
       <li><a href='https://nf-style.vercel.app' className='current'>Home</a></li>
       <li><Link to="container-section1" smooth={true} duration={500}>About</Link></li>
       <li><Link to="container-section2" smooth={true} duration={500}>Services</Link></li>
