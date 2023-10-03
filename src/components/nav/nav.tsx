d4acd3146b33f275914275fcacad1bf351b3bb1f
Mauricios73 <mauricioportela73@gmail.com>
2023-10-03 00:20:29 -0300

    feat: Add login

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -6,6 +6,7 @@ const Nav: React.FC = () => {
   return (
     <ul>
       <li><a href='http://localhost:5173' className='current'>Home</a></li>
+      <li><a href='https://nf-style.vercel.app' className='current'>Home</a></li>
       <li><Link to="container-section1" smooth={true} duration={500}>About</Link></li>
       <li><Link to="container-section2" smooth={true} duration={500}>Services</Link></li>
       <li><Link to="container-section4" smooth={true} duration={500}>Contact</Link></li>
