e4fbe2a07e53d1f3a49b74f4ed01009d51e34a91
Mauricios73 <mauricioportela73@gmail.com>
2024-03-27 15:00:00 -0300

    feat: add validação nos campos de agendamento, toast de error e success

--- src/components/pagLogin/login/pagUsers/pagUser.tsx
+++ src/components/pagLogin/login/pagUsers/pagUser.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './pagUser.css';
 import '../login2.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function PagUser() {
   const [name, setName] = useState<string>('');
@@ -8,6 +10,15 @@ function PagUser() {
   const [email, setEmail] = useState<string>('');
   const [date, setDate] = useState<string>('');
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [formComplete, setFormComplete] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (name.trim() !== '' && isValidPhone(phone) && isValidEmail(email) && date.trim() !== '') {
+      setFormComplete(true);
+    } else {
+      setFormComplete(false);
+    }
+  }, [name, phone, email, date]);
 
   const openModal = () => {
     setShowModal(true);
@@ -17,7 +28,45 @@ function PagUser() {
     setShowModal(false);
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'phone') {
+      const formattedPhone = formatPhoneNumber(value);
+      setPhone(formattedPhone);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'date') {
+      setDate(value);
+    }
+  };
+
+  const isValidPhone = (phone: string) => {
+    const phonePattern = /^\([0-9]{2}\)\s[0-9]{5}-[0-9]{4}$/;
+    return phonePattern.test(phone);
+  };
+
+  const isValidEmail = (email: string) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const formatPhoneNumber = (phoneNumber: string) => {
+    const cleanedPhone = phoneNumber.replace(/\D/g, '');
+    const formattedPhone = cleanedPhone.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+    return formattedPhone;
+  };
+
   const handleSubmit = () => {
+    if (!formComplete) {
+      toast.warning('Por favor, preencha todos os campos corretamente!', {
+        position: 'top-right',
+        className: 'toast-message'
+      });
+      return;
+    }
+
     const formData = {
       name,
       phone,
@@ -32,25 +81,34 @@ function PagUser() {
       },
       body: JSON.stringify(formData)
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Erro ao enviar dados do formulário');
-    })
-    .then(data => {
-      console.log(data);
-      setName('');
-      setPhone('');
-      setEmail('');
-      setDate('');
-      closeModal();
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Erro ao enviar dados do formulário');
+      })
+      .then(data => {
+        console.log(data);
+        setName('');
+        setPhone('');
+        setEmail('');
+        setDate('');
+        closeModal();
 
-      alert(`Nome: ${name}\nTelefone: ${phone}\nEmail: ${email}\nData de Agendamento: ${date}`);
-    })
-    .catch(error => {
-      console.error('Erro:', error);
-    });
+
+        toast.success("Agendamento enviado com sucesso!", {
+          position: 'top-right',
+          className: 'toast-message',
+        });
+
+      })
+      .catch(error => {
+        console.error('Erro:', error);
+        toast.error("Erro ao enviar os dados do formulário!", {
+          position: 'top-right',
+          className: 'toats-message',
+        })
+      });
   };
 
   return (
@@ -71,40 +129,50 @@ function PagUser() {
             <h2>Agendamento</h2>
             <form>
               <label htmlFor="name">Nome:</label>
-              <input type="name" 
-              id="name" 
-              value={name} 
-              onChange={(e) => setName(e.target.value)} 
+              <input type="name"
+                id="name"
+                name="name"
+                value={name}
+                onChange={handleInputChange}
+                required
               /><br />
-              
+
               <label htmlFor="phone">Telefone:</label>
-              <input type="tel" 
-              id="phone" 
-              value={phone} 
-              onChange={(e) => setPhone(e.target.value)} 
+              <input type="tel"
+                id="phone"
+                name="phone"
+                value={phone}
+                onChange={handleInputChange}
+                pattern="\([0-9]{2}\)\s[0-9]{5}-[0-9]{4}"
+                title="Digite um telefone no formato (99) 99999-9999"
+                required
               /><br />
 
               <label htmlFor="email">Email:</label>
-              <input type="email" 
-              id="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
+              <input type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+                required
               /><br />
-              
+
 
               <label htmlFor="date">Data de Agendamento:</label>
-              <input type="date" 
-              id="date" 
-              required value={date} 
-              onChange={(e) => setDate(e.target.value)}
+              <input type="date"
+                id="date"
+                name="date"
+                required value={date}
+                onChange={handleInputChange}
               /><br />
 
-              <button type="button" id="submitFormButton" onClick={handleSubmit}>Enviar</button>
+              <button type="button" id="submitFormButton" onClick={handleSubmit} disabled={!formComplete}>Enviar</button>
               <button type="button" id="cancelButton" className='cancel' onClick={closeModal}>Cancelar</button>
             </form>
           </div>
         </div>
       )}
+      <ToastContainer />
     </div>
   );
 }
